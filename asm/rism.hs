342d41daf2ba74c2af55181de0f90b3da11c71e8
Yumenotimata <kouyayamaoka@gmail.com>
2023-08-20 19:21:54 +0900

    s

--- asm/rism.hs
+++ asm/rism.hs
@@ -6,19 +6,19 @@ genTypeR strList =  [] ++ (drop 4 strList)
     where
         machineCode = []
 
-data InstructionType = Type ([String] -> [String])
+data InstructionType = TYPE_R | TYPE_O
 data Instruction = Instruction {instructionName :: String,instructionType :: InstructionType,func7 :: String,func3 :: String,opcode :: String}
 --instructionSet = [  Instruction {instructionName = "add",instructionType = TYPE_R},
   --                  Instruction {instructionName = "sub",instructionType = TYPE_R}  ]
 
-fromBinaryToDecimal :: [Char] -> Int
+fromBinaryToDecimal :: String -> Int
 fromBinaryToDecimal binary = foldl (\acc x -> 2*acc + x) 0 $ map (\x -> read [x]) binary
 
 main = do
     handle <- openFile "code.asm" ReadMode
     contents <- hGetContents handle
     let token :: [String] = map (flip removeStrListFromStr ["\n"]) $ splitWithDeliChar contents [' ',',','(',')']
-    let func = [genADD,genSUB]
+    -- let func = [genADD,genSUB]
     -- print $ zipWithFunction func token
     print token
     --print $ tokenize token func
@@ -37,13 +37,76 @@ tokenize strList funcList = tokenize' strList funcList [[]]
                 resStrList  = drop 4 result
                 machineCode = take 4 result
 
+mapFunc :: [String] -> [([String] -> [String])] -> [String]
+mapFunc _ [] = [[]]
+mapFunc strList@(str:strList') funcList@(func:funcList')
+    |   null $ head $ result    = mapFunc strList funcList'
+    |   otherwise               = result
+    where
+        result = func strList
+
 genCode :: [String] -> [Instruction] -> [String]
 genCode _ [] = [[]]
 genCode strList@(str:strList') instructionList@(instruction:instructionList')
     |   null $ head $ result    = genCode strList instructionList'
     |   otherwise               = result
     where
-        result = [[]]
+        result = genMachineCode strList instruction
+
+genMachineCode :: [String] -> Instruction -> [String]
+genMachineCode strList@(str:strList') instruction
+    |   instructionName instructionType == TYPE_R   = [[]] 
+    |   otherwise                           = [[]]
+    where
+
+genR :: [String] -> Int -> Int -> Int -> [String]
+genR asm func7 func3 opcode = fromDecimalToBinary $ func7 `shiftL` 25 + rs2 `shiftL` 20 + rs1 `shiftL` 15 + func3 `shiftL` 12 + rd `shiftL` 7 + opcode
+    where
+        rs1 = restoreRegisterNumber $ head $ takePart asm 2 1
+        rs2 = restoreRegisterNumber $ head $ takePart asm 3 1
+        rd  = restoreRegisterNumber $ head $ takePart asm 1 1
+
+fromDecimalToHex :: Int -> String
+fromDecimalToHex de = fromDecimalToHex' de []
+fromDecimalToHex' :: Int -> String -> String
+fromDecimalToHex' de tmp
+    |   remainder /= 0  = fromDecimalToHex' quotient tmp ++ [convTable !! remainder]
+    |   otherwise       = tmp
+    where
+        quotient    = de `div` 16
+        remainder   = de `mod` 16
+        convTable   = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+
+take' :: [a] -> [Int] -> [a]
+take' a b = take'' a b []
+take'' :: [a] -> [Int] -> [a] -> [a]
+take'' _ [] a' = a'
+take'' a b@(_b:b') a' = take'' a b' a' ++ [a !! _b]
+
+takePart :: [a] -> Int -> Int -> [a]
+takepart [] _ _ = []
+takePart a start end = take end $ drop start a
+
+shiftL :: Int -> Int -> Int
+shiftL num sh = 2 ^ sh * num
+
+fromDecimalToBinary :: Int -> [String]
+fromDecimalToBinary deci = [takePart filled 0 2] ++ [takePart filled 2 2] ++ [takePart filled 4 2] ++ [takePart filled 6 2]
+    where
+        hex     = fromDecimalToHex deci
+        filled  = (flip replicate '0' $ 8 - length hex) ++ hex
+
+numLength :: Int -> Int
+numLength num = numLength' num 1
+    where
+        numLength' :: Int -> Int -> Int
+        numLength' num tmp
+            |   num' > 0     = numLength' num' (tmp + 1)
+            |   otherwise    = tmp
+            where
+                num' = num `div` 10
+
+
 
 splitWithDeliChar :: String -> [Char] -> [String]
 splitWithDeliChar str deli = splitWithDeliChar' str [] [[]] deli
